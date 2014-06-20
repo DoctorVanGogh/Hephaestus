0f15a0db6f8565dc27195856e0c05e94a923e74f
DoctorVanGogh <DoctorVanGogh@users.noreply.github.com>
2014-06-20 20:42:31 +0200

    Added german & french localizations

--- Hephaestus/localization/frFR.lua
+++ Hephaestus/localization/frFR.lua
@@ -4,3 +4,17 @@
 -----------------------------------------------------------------------------------------------
 
 local L = Apollo.GetPackage("Gemini:Locale-1.0").tPackage:NewLocale("Hephaestus", "frFR")
+if not L then return end
+L["QueueHeader"] = "Liste de l'artisanat"
+L["QueueStart"] = "Commencer"
+L["QueueStop"] = "Arrêter"
+L["QueueClear"] = "Vider"
+L["RepeatHeader"] = "Ajouter à la liste d'artisanat"
+L["RepeatCostPerItem"] = "Coût par pièce:"
+L["RepeatCostTotal"] = "Du coût total:"
+L["RepeatAdd"] = "Ajouter"
+L["BlockerMounted"] = "Pas monté: %s"
+L["BlockerCraftingStation"] = "À une station d'artisanat: %s"
+L["BlockerAbility"] = "Pas effectuer une action: %s"
+L["BlockerMaterials"] = "Suffisamment de matériaux: %s"
+L["BlockerInventory"] = "Espace d'inventaire assez: %s"
